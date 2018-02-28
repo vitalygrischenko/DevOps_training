18d607c632bba5b7a7b6dcc56d00bd394f0f6bd6
vitalygrischenko <30495687+vitalygrischenko@users.noreply.github.com>
2018-03-01 00:56:52 +0300

    Delete config_server1.sh

--- config_server1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f /home/vagrant/.ssh/id_rsa.pub ]; then
-cat /home/vagrant/.ssh/id_rsa.pub >> /vagrant/server1_vm.pub
-else
-ssh-keygen -b 4096 -t rsa -N ""
-cat /home/vagrant/.ssh/id_rsa.pub >> /vagrant/server1_vm.pub
-fi
-
